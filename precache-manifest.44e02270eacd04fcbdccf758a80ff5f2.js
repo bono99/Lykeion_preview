4fce9a78cb7e44fd953547da12e95f08629b272d
wilmer Petersson <wilmerpettersson6@gmail.com>
2022-09-30 18:39:12 +0900

    Updates

---
+++ precache-manifest.44e02270eacd04fcbdccf758a80ff5f2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0156a29d49678ea25a15601e5c1d1843",
+    "url": "/Lykeion_preview/index.html"
+  },
+  {
+    "revision": "707fc0cae196ae0f6d50",
+    "url": "/Lykeion_preview/static/css/main.54b81e15.chunk.css"
+  },
+  {
+    "revision": "3c70ab532736ef790147",
+    "url": "/Lykeion_preview/static/js/2.554b1aea.chunk.js"
+  },
+  {
+    "revision": "9ab2258d2965e65795ef2389451f17fa",
+    "url": "/Lykeion_preview/static/js/2.554b1aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707fc0cae196ae0f6d50",
+    "url": "/Lykeion_preview/static/js/main.27e8f4dc.chunk.js"
+  },
+  {
+    "revision": "6de2f5839c28580308eb",
+    "url": "/Lykeion_preview/static/js/runtime-main.8e135558.js"
+  },
+  {
+    "revision": "50f5f0f90f7ce11c3621996ad0f31edf",
+    "url": "/Lykeion_preview/static/media/EdmundHusserl.50f5f0f9.mp4"
+  },
+  {
+    "revision": "974953098da0458d52f6390ef537806d",
+    "url": "/Lykeion_preview/static/media/JeremyBentham.97495309.jpg"
+  },
+  {
+    "revision": "b9d88a5475f32b778c2d3c61c8cff106",
+    "url": "/Lykeion_preview/static/media/MichelFoucault.b9d88a54.mp4"
+  },
+  {
+    "revision": "203b2a6bbe37920f07139d1a76fbb7fd",
+    "url": "/Lykeion_preview/static/media/aesthetics.203b2a6b.jpg"
+  },
+  {
+    "revision": "3d238483a2e6a323029d4dc2ab3ee8d7",
+    "url": "/Lykeion_preview/static/media/bg1.3d238483.svg"
+  },
+  {
+    "revision": "7004a6da9848fdbaabe38bc34a36321d",
+    "url": "/Lykeion_preview/static/media/epistemology.7004a6da.jpg"
+  },
+  {
+    "revision": "a5ec5464747117ca07a24deab3e78355",
+    "url": "/Lykeion_preview/static/media/ethics.a5ec5464.jpg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/Lykeion_preview/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "d2e101a63370d442edf8fcb0ca26ea9b",
+    "url": "/Lykeion_preview/static/media/lyceum.d2e101a6.svg"
+  },
+  {
+    "revision": "0fbab2f229e98f6ea02dda613bc961e2",
+    "url": "/Lykeion_preview/static/media/metaphysics.0fbab2f2.jpg"
+  },
+  {
+    "revision": "8297a591a632bd77a4b1717eef813a7a",
+    "url": "/Lykeion_preview/static/media/metaphysics_introduction.8297a591.jpg"
+  },
+  {
+    "revision": "e313145fa243eded2f2c7c14026076ad",
+    "url": "/Lykeion_preview/static/media/philosophyofmind.e313145f.jpg"
+  },
+  {
+    "revision": "aea662e84e51a698c5894ca56b469275",
+    "url": "/Lykeion_preview/static/media/postmodernism.aea662e8.jpg"
+  },
+  {
+    "revision": "08238ce7de351c67fe4034d052b9c086",
+    "url": "/Lykeion_preview/static/media/sartre.08238ce7.mp4"
+  }
+]);
